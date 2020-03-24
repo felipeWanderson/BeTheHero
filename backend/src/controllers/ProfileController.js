d1e7ef321e2367c914b29de540ddefaf01049b21
felipeWanderson <felipewanderson2020@gmail.com>
2020-03-24 15:13:59 -0300

    backend da aplicação

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,26 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(request, response) {
+    const ong_id = request.headers.authorization;
+
+    const [count] = await connection('incidents')
+      .where('ong_id', ong_id)
+      .count();
+
+    const incidents = await connection('incidents')
+    .where('ong_id', ong_id)
+    .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+    .select([
+      'incidents.*',
+      'ongs.name',
+      'ongs.email',
+      'ongs.whatsapp',
+      'ongs.city',
+      'ongs.uf',
+    ]);
+
+    response.header('X-Total-Count', count['count(*)']);
+    return response.json(incidents);
+  },
+};
